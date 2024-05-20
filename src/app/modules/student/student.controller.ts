fb6ee955565ab43fc06b9893333731fffea79da2
sabilar-rahman <sabilarrahman36@gmail.com>
2024-05-21 03:41:13 +0600

    Create Student Features

--- src/app/modules/student/student.controller.ts
+++ src/app/modules/student/student.controller.ts
@@ -2,22 +2,54 @@ import { Request, Response } from "express";
 import { StudentServices } from "./student.service";
 
 const createStudent = async (req: Request, res: Response) => {
-    try {
-        const student = req.body;
-        // will call server func to send this data
-        const result = await StudentServices.createStudentIntoDB(student);
+  try {
+    const { student: studentData } = req.body;
+    // will call server func to send this data
+    const result = await StudentServices.createStudentIntoDB(studentData);
 
-        // send Response
-        res.status(200).json({
-            success: true,
-            message: "student is created",
-            data: result,
-        });
-    } catch (err) {
-        console.log(err);
-    }
+    // send Response
+    res.status(200).json({
+      success: true,
+      message: "student is created",
+      data: result,
+    });
+  } catch (err) {
+    console.log(err);
+  }
 };
 
-export const StudentController ={
-    createStudent
-}
+const getAllStudents = async (req: Request, res: Response) => {
+  try {
+    const result = await StudentServices.getAllStudentFromDB();
+
+    res.status(200).json({
+      success: true,
+      message: "students are Retrieved Successfully",
+      data: result,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const getSingleStudent = async (req: Request, res: Response) => {
+  try {
+    const { studentId } = req.params;
+
+    const result = await StudentServices.getSingleStudentFromDB(studentId);
+
+    res.status(200).json({
+      success: true,
+      message: "students are Retrieved Successfully",
+      data: result,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const StudentController = {
+  createStudent,
+  getAllStudents,
+  getSingleStudent,
+};
--- src/app/modules/student/student.route.ts
+++ src/app/modules/student/student.route.ts
@@ -1,6 +1,12 @@
-import express from "express"
-import { StudentController } from "./student.controller"
+import express from "express";
+import { StudentController } from "./student.controller";
 
-const router = express.Router()
+const router = express.Router();
 
-router.post('/create-student',StudentController.createStudent)
+router.post("/create-student", StudentController.createStudent);
+
+router.get("/", StudentController.getAllStudents);
+
+router.get("/:studentId", StudentController.getSingleStudent);
+
+export const StudentRoutes = router;
