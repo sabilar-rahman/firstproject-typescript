f8cca9f147d2202258a710c1a313b3b5cc1663fa
sabilar-rahman <sabilarrahman36@gmail.com>
2024-05-20 09:48:15 +0600

    files

---
+++ src/app.ts
@@ -0,0 +1,21 @@
+import express, { Application, Request, Response } from "express";
+
+import cors from "cors";
+const app: Application = express();
+// const port = 3000
+
+// Parser start
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!");
+});
+
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`)
+// })
+
+// console.log(process.cwd());
+
+export default app;
---
+++ src/app/modules/student/student.interface.ts
@@ -0,0 +1,47 @@
+import { Schema, model, connect } from 'mongoose';
+
+// 1. Create an interface representing a document in MongoDB.
+export type Guardian ={
+    fatherName:string;
+    fatherOccupation:string;
+    fatherContactNo:string;
+
+    motherName:string;
+    motherOccupation:string;
+    motherContactNo:string
+}
+
+export type UserName={
+        firstName: string;
+        middleName: string;
+        lastName: string;
+    }
+
+    export type localGuardian={
+        name: string;
+        occupation: string;
+        contactNo: string;
+        address:string;
+    }
+
+
+
+export type Student = {
+    id:string,
+
+    name:UserName ,
+    gender:"male"|"female",
+    dob:string,
+    email: string;
+    contactNo:string;
+    emergencyContactNo:string;
+    bloodGroup?: "A+" | "A-" | "B+" | "B-" | "AB+" | "AB-" | "O+" | "O-";
+    presentAddress:string;
+    permanentAddress:string;
+    guardian:Guardian;
+    localGuardian:localGuardian;
+    profileImage?:string;
+    isActive:'active'|'inactive'
+
+
+}
